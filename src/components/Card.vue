2d3d308f376ccedae21c314363a411280114a8e5
Ramazan <101656313+JED-Studio@users.noreply.github.com>
2024-09-15 02:49:30 +0300

    Card
    -Добавил карточки и стили

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+    style="width: 350px; height: 407px"
+  >
+    <img class="absolute top-3 left-8" />
+
+    <img alt="Sneaker" />
+    <p class="mt-2">peta</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>lol</b>
+      </div>
+
+      <img alt="Plus" />
+    </div>
+  </div>
+</template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,53 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import Card from './Card.vue'
+
+export default defineComponent({
+  components: {
+    Card
+  },
+
+  setup() {
+    const items = ref([
+      {
+        id: 1,
+        title: 'peta',
+        price: 100,
+        imageUrl: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 2,
+        title: 'peta',
+        price: 100,
+        imageUrl: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 3,
+        title: 'peta',
+        price: 100,
+        imageUrl: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 4,
+        title: 'peta',
+        price: 100,
+        imageUrl: 'https://picsum.photos/200/300'
+      },
+      {
+        id: 5,
+        title: 'peta',
+        price: 100,
+        imageUrl: 'https://picsum.photos/200/300'
+      }
+    ])
+
+    return { items }
+  }
+})
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-center gap-8">
+    <card v-for="item in items" :key="item.id" :item="item" />
+  </div>
+</template>
